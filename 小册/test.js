e688fdce83dc295b766f243f32143798e9ade973
xxx-lxl <1611973362@qq.com>
2023-05-18 20:13:37 +0800

    完结

---
+++ 小册/test.js
@@ -0,0 +1,32 @@
+/* function Decorator(target) {
+  console.log(target)
+  target.hasDecorator = true
+  return target
+}
+
+@Decorator
+class Button {}
+// 验证装饰器是否生效
+console.log('Button 是否被装饰了：', Button.hasDecorator) */
+
+// 具体的参数意义，在下个小节，这里大家先感知一下操作
+function funcDecorator(target, name, descriptor) {
+  console.log(target, name, descriptor)
+  let originalMethod = descriptor.value
+  descriptor.value = function () {
+    console.log('我是Func的装饰器逻辑')
+    return originalMethod.apply(this, arguments)
+  }
+  return descriptor
+}
+
+class Button {
+  @funcDecorator
+  onClick() {
+    console.log('我是Func的原有逻辑')
+  }
+}
+
+// 验证装饰器是否生效
+const button = new Button()
+button.onClick()
